3b1d4b25c7c4f96d685ce3f2d3d0a9370a954849
Sykda <93369567+Sykda@users.noreply.github.com>
2022-01-12 09:19:04 +0100

    commit

--- src/com/psp/ejercicio3/CuentaPares.java
+++ src/com/psp/ejercicio3/CuentaPares.java
@@ -2,33 +2,41 @@ package com.psp.ejercicio3;
 
 import java.util.Date;
 
-public class CuentaPares {
-	
-		
-	public static void main(String[] args) {
-		
-		int n = 0;
-		int x = 1;
-		int y = 100000000;
+public class CuentaPares implements Runnable {
 
-		long t0 = (new Date()).getTime();
+	int n = 0;
+	int x = 1;
+	int y = 0;// 100000000
 
-		for (int i = x; i <= y; i++) {
-			
-		
-			if (i % 2 == 0) {
-				n++; 
-				System.out.println(i+" es par");
-			}
-			
-		}
-		
-		long t1 = (new Date()).getTime();
-		long time = t1 - t0;
-		
-		System.out.println("Número de pares en el intervalo: "+x+" - "+y+" es igual a "+n +" calculado en "+time+ " milisegundos");
+	long t0 = (new Date()).getTime();
 
+	public CuentaPares(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
 	}
 
-		
+	@Override
+	public void run() {
+
+		for (int i = x; i <= y; i++) {
+
+			if (i % 2 == 0) {
+				n++;
+				System.out.println(i + " es par");
+			}
+
+		}
+
+		long t1 = (new Date()).getTime();
+		long time = t1 - t0;
+
+		System.out.println("Número de pares en el intervalo: " + x + " - " + y + " es igual a " + n + " calculado en "
+				+ time + " milisegundos");
+
+	}
+	
+	
+	
+
 }
